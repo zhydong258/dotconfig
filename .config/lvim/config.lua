2f983f135d5071ad783a86f446ce1ba5969578ab
Ethan Dong <ethan.dong@outlook.com>
2023-05-09 13:23:46 +0800

    Modify config file of lvim.
    Add gui font setting of lvim for neovim-qt.
    Modify .gitconfig for proxy.
    Modify .set_proxy to get IP from WSL2.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -14,6 +14,8 @@ lvim.format_on_save.enabled = false
 lvim.colorscheme = "lunar"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
+-- set font for neovim-qt
+vim.opt.guifont = "MesloLGM Nerd Font:h12"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
